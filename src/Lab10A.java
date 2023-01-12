6bbbf2c0dd007391395adf2a7959e532ac40d55d
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2023-01-12 18:38:07 -0500

    Initial Commit

--- src/Lab10A.java
+++ src/Lab10A.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Lab10A {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Scanner;
+class Lab10A {
+
+    //minimum method
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    //maximum method
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    //average method
+    public static int average(int a, int b) {
+        return (((a + b)) / 2);
+    }
+
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in);
+        int intFirst, intSecond;
+
+        //initial output and get input
+        System.out.print("Enter number 1: ");
+        intFirst = scan.nextInt();
+        System.out.print("Enter number 2: ");
+        intSecond = scan.nextInt();
+
+        //final output calling methods
+        System.out.println("Min is " + min(intFirst, intSecond));
+        System.out.println("Max is " + max(intFirst, intSecond));
+        System.out.println("Average is " + average(intFirst, intSecond));
+
+    }
 }
--- src/Lab10B.java
+++ src/Lab10B.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Lab10B {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Scanner;
+class Lab10B {
+
+    //valid check
+    public static boolean isValid(double w, double h) {
+        return (w + h) > 30;
+    }
+
+    //area
+    public static double area(double w, double h) {
+        return (w * h);
+    }
+
+    //perimeter
+    public static double perimeter(double w, double h) {
+        return (2 * w + 2 * h);
+    }
+
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in);
+        double dblWidth, dblHeight;
+        boolean boolStop = false;
+        char charStop;
+
+        //begin loop
+        while (!boolStop) {
+
+            //initial output and get input
+            System.out.print("Enter width: ");
+            dblWidth = scan.nextDouble();
+            System.out.print("Enter height: ");
+            dblHeight = scan.nextDouble();
+
+            //call valid check
+            if (isValid(dblWidth, dblHeight) == false) {
+                System.out.println("This is an invalid rectangle");
+            }
+            else {
+                System.out.println("This is a valid rectangle");
+                System.out.println("The area is: " + area(dblWidth, dblHeight));
+                System.out.println("The perimeter is: " + perimeter(dblWidth, dblHeight));
+            }
+            System.out.println("Do you want to enter another width and height (Y/N)? : ");
+            charStop = scan.next().charAt(0);
+            if (charStop == 'N' || charStop == 'n') {
+                boolStop = true;
+                System.out.println("Program Ends");
+            }
+
+        }
+    }
 }
